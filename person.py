b40c6fd80c634370291f7c8775f8ae7a755047fe
Desalew <dessalew186@gmail.com>
2025-12-27 20:27:01 -0600

    Remove submodule and add Python code normally

--- person.py
+++ person.py
@@ -24,7 +24,7 @@ class person:
     def update_age(self, new_age):
         self.age=new_age
         print(f"New age is: {self.age}")
-#new method added to trigger the action
+#new
 
 p1=person("Alex", 41, 2025)
 p1.greet()
